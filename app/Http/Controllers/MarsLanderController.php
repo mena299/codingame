aafb2ba102eab4eac2bb378d58530e707192a2e4
Kanokwan Saeung <lawmena299@gmail.com>
2017-12-15 14:41:11 +0700

    first 100%

---
+++ app/Http/Controllers/MarsLanderController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MarsLanderController extends Controller
+{
+    public function levelOne()
+    {
+        fscanf(STDIN, "%d",
+            $surfaceN // the number of points used to draw the surface of Mars.
+        );
+        for ($i = 0; $i < $surfaceN; $i++) {
+            fscanf(STDIN, "%d %d",
+                $landX, // X coordinate of a surface point. (0 to 6999)
+                $landY // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+            );
+        }
+
+
+// game loop
+        while (TRUE) {
+            fscanf(STDIN, "%d %d %d %d %d %d %d",
+                $X,
+                $Y,
+                $hSpeed, // the horizontal speed (in m/s), can be negative.
+                $vSpeed, // the vertical speed (in m/s), can be negative.
+                $fuel, // the quantity of remaining fuel in liters.
+                $rotate, // the rotation angle in degrees (-90 to 90).
+                $power // the thrust power (0 to 4).
+            );
+
+            // Write an action using echo(). DON'T FORGET THE TRAILING \n
+            // To debug (equivalent to var_dump): error_log(var_export($var, true));
+
+            $power = 0;
+            if ($vSpeed <= -40) {
+                $power = 4;
+            }
+
+            // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+            echo("0 $power\n");
+        }
+    }
+
+}
